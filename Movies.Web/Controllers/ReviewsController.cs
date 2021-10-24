ca831076bc4cefd725b1a0dfb163c282750ed4fe
Tyrone Davison <t.p.davison@tees.ac.uk>
2021-10-24 19:08:02 +0100

    Add placeholder readonly controller

---
+++ Movies.Web/Controllers/ReviewsController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.Web.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
