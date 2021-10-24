124e21d2bba86b41b43076b666a0a4dd716fe13e
Tyrone Davison <t.p.davison@tees.ac.uk>
2021-10-24 20:00:18 +0100

    Update app controller to use injected service

--- Movies.Web/Startup.cs
+++ Movies.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Movies.Web.Services;
 
 namespace Movies.Web
 {
@@ -23,6 +24,8 @@ namespace Movies.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<IReviewsService, ReviewsService>();
+
             services.AddControllersWithViews();
         }
 
